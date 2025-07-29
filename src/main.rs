985ad999b22b63e57a5ae40de1e1b139f0d20a58
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-29 15:50:07 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
+mod command_processing;
+mod gambling;
 
+use clap::Parser;
+use command_processing::Cli;
+
+fn main() {
+    let cli = Cli::parse();
+    println!("command: {:?}", cli.command);
+}
