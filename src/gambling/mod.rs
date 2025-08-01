de3225513827494b9a1f4b48264006f1932cf8d6
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-08-02 00:00:30 +0200

    Update mod.rs

--- src/gambling/mod.rs
+++ src/gambling/mod.rs
@@ -1,1 +1,12 @@
+mod coinflip;
+pub mod gamble_result;
+
+use coinflip::coinflip;
+use rand::random;
+pub fn rand_gamble_with_difficulty(difficulty: u8) -> GambleResult {
+    match random::<u32>() % 1 {
+        0 => coinflip(difficulty),
+        _ => unreachable!(),
+    }
+}
 
