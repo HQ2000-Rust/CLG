d2cd09ff5e0ac79e11317a57033458bcc9c64ea8
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-08-08 19:58:15 +0200

    Update mod.rs

--- src/gambling/mod.rs
+++ src/gambling/mod.rs
@@ -1,14 +1,17 @@
-mod ascii_art;
+pub mod ascii_art;
 mod coinflip;
+pub mod machine;
 mod utils;
 
 pub use utils::GambleResult;
 
+use crate::gambling::machine::machine;
 use coinflip::coinflip;
 use fastrand;
-pub fn rand_gamble_with_difficulty(difficulty: u8) -> std::io::Result<GambleResult> {
-    Ok(match fastrand::u8(..1) {
-        0 => coinflip(difficulty)?,
-        _ => unreachable!(),
+
+pub fn rand_gamble() -> std::io::Result<GambleResult> {
+    Ok(match fastrand::bool() {
+        true => coinflip()?,
+        false => machine()?,
     })
 }
